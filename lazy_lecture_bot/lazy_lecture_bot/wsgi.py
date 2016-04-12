971216cf6a472ca7a16dcf511df23a83efa76dc0
brianecker <briancecker@gmail.com>
2016-04-11 18:49:17 -0700

    Added a modules package and modified the system path in wsgi.py to include all of lazy_lecture_bot/*.

--- lazy_lecture_bot/lazy_lecture_bot/wsgi.py
+++ lazy_lecture_bot/lazy_lecture_bot/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
+import sys
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+# https://community.webfaction.com/questions/3553/django-cant-find-my-module
+sys.path = [os.path.abspath(os.path.join(__file__, "../.."))] + sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lazy_lecture_bot.settings")
 
 application = get_wsgi_application()
