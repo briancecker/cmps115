46e4204ea365d6592b7ac4309e06c1aac03f1229
brianecker <briancecker@gmail.com>
2016-05-19 22:10:39 -0700

    Fixed initial_data_loading.py to only try to load when the pipeline types table exists.

--- lazy_lecture_bot/lazy_lecture_bot/initial_data_loading.py
+++ lazy_lecture_bot/lazy_lecture_bot/initial_data_loading.py
@@ -5,12 +5,15 @@ from django.core.management import call_command
 
 
 # The app's that have fixtures you want to load data from
+from django.db import connection
+
 FIXTURES = [("main", "fixtures", "initial_data.json")]
 
 
 def import_data():
-    for fixture in FIXTURES:
-        call_command("loaddata", os.path.join(*fixture))
+    if "main_pipelinetypes" in connection.introspection.table_names():
+        for fixture in FIXTURES:
+            call_command("loaddata", os.path.join(*fixture))
 
 
 
