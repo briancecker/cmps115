c40bfa84bac8ecb3775289f4ed35c71ae8fb20b0
brianecker <briancecker@gmail.com>
2016-05-23 16:36:24 -0700

    Fixed a bug and typos in search_indexes.

--- lazy_lecture_bot/main/search_indexes.py
+++ lazy_lecture_bot/main/search_indexes.py
@@ -33,15 +33,16 @@ class UtteranceIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
- class videoIndex(indexes.SearchIndex, indexes.Indexable):
-     text = indexes.CharField(document=True, use_template=True)
-     title = indexes.CharField(model_attr="title")
-     video_id - indexes.IntegerField(model_attr="id")
- 
-     def get_model(self):
-         return Videos
- 
-     def index_queryset(self, using=None):
-         return self.get_model().objects.all()
+
+class VideoIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr="title")
+    video_id = indexes.IntegerField(model_attr="id")
+
+    def get_model(self):
+        return Videos
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
  
 
